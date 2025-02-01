49fcc299983ae6b6ab8f7a4fe5b4d52a5c65679d
JpenalozaDuoc <163478180+JpenalozaDuoc@users.noreply.github.com>
2025-02-01 00:05:29 -0300

    primera carga del proyecto

---
+++ dcn-producer-alerts/productor-alertas/src/main/java/productor_alerts/productor_alertas/service/ProductorService.java
@@ -0,0 +1,16 @@
+package productor_alerts.productor_alertas.service;
+
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductorService {
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+
+    public void sendMessage(String message){
+        rabbitTemplate.convertAndSend("myQueue", message);
+    }
+
+}
